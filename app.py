b9ca701917ae24b27ed14e6b79fe589f85ef8cad
Konstantinos Stamou (Dinos) <github@f3rr3t.net>
2021-07-13 18:40:27 +0300

    Comments added

--- app.py
+++ app.py
@@ -2,37 +2,49 @@ from flask import Flask, request, Response, json, redirect, url_for
 import hashlib
 from dicttoxml import dicttoxml
 
+# Initialization of Flask application, you can add here a secret key
 app = Flask(__name__)
 app.secret_key = "super secret key"
+
+# Dictionary initialization
 url_storage = {}
 
+# Shorten endpoint
 @app.route('/api/v1/shorten/<path:subpath>', methods=['POST'])
 
 def shorten(subpath):
     url = subpath
     
+    # Check if the URL starts with https or http
     if not url.startswith(("http://", "https://")):
       return Response("http:// or https:// needed ", 400)
-
+    
+    # Hash the URL with SHA256
     hs = hashlib.sha256(url.encode('utf-8')).hexdigest()
     
+    # Store it to the dictionary
     url_storage.update({hs[0:10]:url})
 
+    # Create the shortened URL
     short_url = "https://domain.ltd/" + hs[0:10]
     
+    # Create the data to be sent as response
     data = {"url" : short_url}
-
+    
+    # Return the XML Response if needed
     if request.content_type == 'application/xml':
       xmldata = dicttoxml(data, custom_root='data', attr_type=False)
       return Response(xmldata, mimetype='application/xml')
     
+    # Standard JSON response
     return Response(json.dumps(data), mimetype='application/json')
     
-
+# Lookup endpoint creation
 @app.route('/api/v1/lookup/<identifier>', methods=['GET'])
 
 def lookup(identifier):
 
+    # Lookup of the key to the dictionary
     try:
       lookup_key = url_storage[identifier]
       data = {"original_url" : lookup_key}
@@ -40,13 +52,15 @@ def lookup(identifier):
     except KeyError:
       return Response("No url found!", 400)
     
+    # XML Response if needed
     if request.content_type == 'application/xml':
       xmldata = dicttoxml(data, custom_root='data', attr_type=False)
       return Response(xmldata, mimetype='application/xml')
-
+    
+    # Standard JSON response
     return Response(json.dumps(data), mimetype='application/json')
     
-
+# Redirect endpoint
 @app.route('/<id>', methods=['GET'])
 
 def original(id):
